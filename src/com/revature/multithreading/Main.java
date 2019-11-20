51e7b294e0769d214166ab51283574a4e7be87e8
Keyneloui <keyne.loui@gmail.com>
2019-11-20 19:04:59 +0530

    implemented threads

--- src/com/revature/multithreading/Main.java
+++ src/com/revature/multithreading/Main.java
@@ -1,25 +1,30 @@
 package com.revature.multithreading;
 
 import java.util.Random;
+import java.util.Scanner;
 
-class Example extends Thread {
+class Example implements Runnable {
 	private enum Coin {
 		Heads, Tails
 	};
 
-	public void run() {
+	synchronized public void run() {
 		int headCount = 0;
 		int tailCount = 0;
 		int probability = 0;
-		for (int i = 1; i <= 5; i++) {
+		System.out.println("Enter the no of times you want to flip the coin");
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for (int i = 1; i <= n; i++) {
 			Random randomNumber = new Random();
 			int result = randomNumber.nextInt(2);
-
+			System.out.println("Result=>"+result);
+			System.out.println("Thread name=>"+Thread.currentThread().getName());
 			if (result == 0) {
 				System.out.println(Coin.Heads);
 				System.out.println("Heads flipped");
 				headCount = headCount + 1;
-
+				
 			} else {
 				System.out.println(Coin.Tails);
 				System.out.println("Tails flipped");
@@ -28,9 +33,10 @@ class Example extends Thread {
 			}
 
 		}
+		scn.close();
 		System.out.println("tails flipped:" + tailCount);
 		System.out.println("Heads flipped:" + headCount);
-		probability = (headCount + tailCount) / 5;
+		probability = (headCount + tailCount) / n;
 		if (probability == 1) {
 			System.out.println("probability is 1");
 		} else {
@@ -43,11 +49,15 @@ class Example extends Thread {
 public class Main {
 	public static void main(String[] args) {
 		Example example = new Example();
-		example.start();
+		Thread tobj = new Thread(example);
+		tobj.setName("car");
+		tobj.start();
 //		example.setName("HEADS");
 //		example.setPriority(1);
-//		Example example1 = new Example();
-//		example1.start();
+		Example example1 = new Example();
+		Thread threadObj = new Thread(example1);
+	threadObj.setName("cycle");
+		threadObj.start();
 //		example1.setName("Tails");
 //		example1.setPriority(10);
 
