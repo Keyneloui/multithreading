f8bf9ea2c395d86edb07606c23099c582386177b
Keyneloui <keyne.loui@gmail.com>
2019-11-21 15:02:12 +0530

    implemented enum naming conventions and shorthand operators

--- src/com/revature/multithreading/Main.java
+++ src/com/revature/multithreading/Main.java
@@ -5,35 +5,37 @@ import java.util.Scanner;
 
 class Example implements Runnable {
 	private enum Coin {
-		Heads, Tails
+		HEADS, TAILS
 	};
 
-	synchronized public void run() {
+	 public void run() {
 		int headCount = 0;
 		int tailCount = 0;
 		int probability = 0;
-		System.out.println("Enter the no of times you want to flip the coin");
-		Scanner scn = new Scanner(System.in);
-		int n = scn.nextInt();
+//		System.out.println("Enter the no of times you want to flip the coin");
+//		Scanner scn = new Scanner(System.in);
+		int n = 5;
 		for (int i = 1; i <= n; i++) {
 			Random randomNumber = new Random();
 			int result = randomNumber.nextInt(2);
 			System.out.println("Result=>"+result);
-			System.out.println("Thread name=>"+Thread.currentThread().getName());
+		
 			if (result == 0) {
-				System.out.println(Coin.Heads);
+				System.out.println("Thread name=>"+Thread.currentThread().getName());
+				System.out.println(Coin.HEADS);
 				System.out.println("Heads flipped");
-				headCount = headCount + 1;
+				headCount ++;
 				
 			} else {
-				System.out.println(Coin.Tails);
+				System.out.println("Thread name=>"+Thread.currentThread().getName());
+				System.out.println(Coin.TAILS);
 				System.out.println("Tails flipped");
-				tailCount = tailCount + 1;
+				tailCount ++;
 
 			}
 
 		}
-		scn.close();
+		//scn.close();
 		System.out.println("tails flipped:" + tailCount);
 		System.out.println("Heads flipped:" + headCount);
 		probability = (headCount + tailCount) / n;
@@ -42,24 +44,32 @@ class Example implements Runnable {
 		} else {
 			System.out.println("Conditon failed");
 		}
-
+		System.out.println("----END----");
 	}
 }
 
 public class Main {
 	public static void main(String[] args) {
 		Example example = new Example();
+		System.out.println("Enter the no of coin");
+		Scanner scn = new Scanner(System.in);
+		int n = scn.nextInt();
+		for(int a=0;a<n;a++)
+		{
 		Thread tobj = new Thread(example);
-		tobj.setName("car");
+		tobj.setName("Thread-a"+a);
 		tobj.start();
+		}
+		scn.close();
 //		example.setName("HEADS");
-//		example.setPriority(1);
-		Example example1 = new Example();
-		Thread threadObj = new Thread(example1);
-	threadObj.setName("cycle");
-		threadObj.start();
+////		example.setPriority(1);
+//		Example example1 = new Example();
+//		Thread threadObj = new Thread(example1);
+//	threadObj.setName("cycle");
+//		threadObj.start();
 //		example1.setName("Tails");
 //		example1.setPriority(10);
 
 	}
+	
 }
